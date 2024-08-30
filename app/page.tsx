3a8c7b66f53abee4e7a6237fa5febdd63d3bb0af
scyoxe <scyoxe@gmail.com>
2024-08-30 16:50:27 +0300

    Making the navigatiion bar responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+import { useState } from 'react';
 import Brand from './components/Brand';
 import Header from './components/Header';
 import Main from './components/Main';
@@ -8,10 +10,55 @@ import styles from './page.module.css';
 import Footer from './components/Footer';
 
 export default function Landing() {
+    const [menu, setMenu] = useState(true);
+    function toogleMenu() {
+        setMenu(!menu);
+    }
     return (
         <>
             <Header>
                 <Brand />
+                <div className={styles.menu}>
+                    <div
+                        onClick={toogleMenu}
+                        className={styles.menu_image}>
+                        <Image
+                            src={'./images/menu.svg'}
+                            alt='Menu Icon'
+                            width={50}
+                            height={50}
+                        />
+                    </div>
+                    {menu ? (
+                        <div></div>
+                    ) : (
+                        <div className={styles.menu_nav}>
+                            <NavItem
+                                style='active'
+                                name='Landing'
+                                link='/'
+                            />
+
+                            <NavItem
+                                name='Docs'
+                                link='/docs'
+                            />
+                            <div className={styles.github_repository}>
+                                <a
+                                    href='https://github.com/scyoxe/authify'
+                                    target='_blank'>
+                                    <Image
+                                        src={'./images/github.svg'}
+                                        alt='Github Logo'
+                                        width={50}
+                                        height={50}
+                                    />
+                                    <p>GitHub</p>
+                                </a>
+                            </div>
+                        </div>
+                    )}
+                </div>
                 <Nav>
                     <NavItem
                         style='active'
